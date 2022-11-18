8b06e37901f8d02f6aa3253b86d74b91c85e51ca
Abhishek Soni <117580207+Abhis0ni@users.noreply.github.com>
2022-11-18 22:17:43 +0530

    Create For_While_loop.py

---
+++ For_While_loop.py
@@ -0,0 +1,56 @@
+
+# For loop
+for num in range(0,11): #range(0,11) --> [0,1,2,3,4,5,6,7,8,9,10]
+    print(num)
+
+for num in range(0,11,2): # with step=2 
+    print(num)
+
+for num in range(10,0,-1):
+    print(num)
+
+
+list1 =[30,'Abhi',30.5,[1,2,3]]
+for num in list1:
+    print(num)
+
+list2 = [12, 75, 150, 180, 145, 525, 50]
+total = 0
+for num in list2:
+    total=total+num
+print('Total from for loop : ',total)
+
+# Break 
+# Print first even number from list and exit
+list3 = [1, 3, 5, 8, 10, 7, 11]
+for num in list3:
+    if(num%2==0):
+        print('Even :',num)
+        break
+
+
+# Continue
+# Print numbers from list greater than 10
+list4 = [1, 12, 5, 8, 10, 7, 11]
+for num in list4:
+    if (num <= 10):
+        continue
+    print("Numbers >10 :",num)
+
+
+
+
+# While loop
+counter=1
+while counter<=11: # Either True or False
+    print(counter)
+    counter+=1
+
+
+list2 = [12, 75, 150, 180, 145, 525, 50]
+total=0
+count=0
+while count<len(list2):
+    total=total+list2[count]
+    count+=1
+print("Total from while loop :",total)
