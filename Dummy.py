b075dc37cbe656ec03f89f7fe345e70f3f405d3c
Abhis0ni <abhi232157@yahoo.com>
2022-11-27 16:43:00 +0530

    byee

--- Dummy.py
+++ Dummy.py
@@ -7,20 +7,3 @@ T1=(123,'werre',23.32)
 print(T1+T2)
 
 
-# Tuple slicing - works same as string
-T6=('hi',2022,32.8,'SAP')
-print(T6[0:2])
-print(T6[::-1]) #Reverse whole tuple
-print(T6[-1::-1]) #Reverse whole tuple
-# Tuple does't support T6.reverse()
-print(T6)
-
-
-# Tuple slicing - works same as string
-T6=('hi',2022,32.8,'SAP')
-print(T6[0:2])
-print(T6[::-1]) #Reverse whole tuple
-print(T6[-1::-1]) #Reverse whole tuple
-# Tuple does't support T6.reverse()
-print(T6)
-
